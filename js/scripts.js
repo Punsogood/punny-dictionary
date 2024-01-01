8590140c16af1ba4e848bdc9277172c76c6d4559
Punsogood <142711965+Punsogood@users.noreply.github.com>
2024-01-01 13:27:48 -0800

    Pokemon Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-alert('Hello world');
-let = 'favoriteFood';
-favoriteFood = 'chocolate';
-document.write (favoriteFood);
+let pokemonList = [{name: "Tranquill", height: "0.6", types: ["rivalry", "superluck", "bigpecks"] }]
+[{name: "Butterfree", height: "1.1", types: [compoundeyes, tinted-lens]}]
+[{name: "Slowpoke", height: "1.2", types: ["oblivious", "own-tempo", "regenerator"]}]
