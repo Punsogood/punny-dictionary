de2f3c6489e4213f3d446c9c35db2e14a19400ae
Punsogood <142711965+Punsogood@users.noreply.github.com>
2024-01-01 13:37:29 -0800

    Pokemon Script Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-let pokemonList = [{name: "Tranquill", height: "0.6", types: ["rivalry", "superluck", "bigpecks"] }]
-[{name: "Butterfree", height: "1.1", types: [compoundeyes, tinted-lens]}]
-[{name: "Slowpoke", height: "1.2", types: ["oblivious", "own-tempo", "regenerator"]}]
+let pokemonList = [{name: "Tranquill", height: "0.6", types: ["rivalry", "superluck", "bigpecks"] }];
+[{name: "Butterfree", height: "1.1", types: ["compoundeyes", "tinted-lens"]}];
+[{name: "Slowpoke", height: "1.2", types: ["oblivious", "own-tempo", "regenerator"]}];
