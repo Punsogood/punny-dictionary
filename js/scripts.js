b1e4b0c84740f55bf34378a669b6bf2676e88e90
Punsogood <142711965+Punsogood@users.noreply.github.com>
2024-01-17 08:57:15 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-let pokemonList = [{name: "Tranquill", height: "0.6", types: ["rivalry", "superluck", "bigpecks"] },
-{name: "Butterfree", height: "1.1", types: ["compoundeyes", "tinted-lens"]},
-{name: "Slowpoke", height: "1.2", types: ["oblivious", "own-tempo", "regenerator"]}];
+let pokemonList = [
+  {
+    name: "Tranquill", 
+    height: "0.6", 
+    types: ["rivalry", "superluck", "bigpecks"],
+  },
+  {
+    name: "Butterfree", 
+    height: "1.1", 
+    types: ["compoundeyes", "tinted-lens"],
+  },
+  {
+    name: "Slowpoke", 
+    height: "1.2", 
+    types: ["oblivious", "own-tempo", "regenerator"],
+  },
+];
