0687281896b0228adfbbab9c382741874eb9c34c
Punsogood <142711965+Punsogood@users.noreply.github.com>
2024-01-17 12:04:36 -0800

    Updates with css

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,28 @@ let pokemonList = [
     height: "0.6", 
     types: ["rivalry", "superluck", "bigpecks"],
   },
+
   {
     name: "Butterfree", 
     height: "1.1", 
     types: ["compoundeyes", "tinted-lens"],
   },
+
   {
     name: "Slowpoke", 
     height: "1.2", 
     types: ["oblivious", "own-tempo", "regenerator"],
   },
+  
 ];
 
 for (let i=0; i<pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + '')
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')')
   if (pokemonList[i].height <=1.1) {
   document.write('<zb>');
   }
   else{
-    document.write('That\'s a big pokemon!');
+    document.write('That\'s a huge pokemon!');
     document.write('<zb>');
   }
   }
