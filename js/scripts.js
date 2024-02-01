3f7c296211d4d48926adf1150442ff8003c74552
Punsogood <142711965+Punsogood@users.noreply.github.com>
2024-02-01 11:15:01 -0800

    Update scripts.js
    forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,13 @@ let pokemonList = [
 
 ];
 
-for (let i=0; i<pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')')
+function myLoopFunction(pokemonList) {
+  document.write(pokemonList[i].name + ' (height: ' +pokemonList[i].height +')')
   if (pokemonList[i].height <=1.1) {
-  document.write('<br>');
-  }
-  else{
-    document.write('That\'s a huge pokemon!');
     document.write('<br>');
-  }
-  }
-
+    }
+    else{
+      document.write('That\'s a huge pokemon!');
+      document.write('<br>');
+    }
+}
